b4ceee9ee44935d436058f60c565c40216017c89
GKrafft2 <guillaume.krafft2@gmail.com>
2024-05-11 11:48:44 +0200

    local llama3 version

--- main.py
+++ main.py
@@ -1,47 +1,37 @@
-from dotenv import load_dotenv
-import os
-import pandas as pd
-from llama_index.query_engine import PandasQueryEngine
-from prompts import new_prompt, instruction_str, context
-from note_engine import note_engine
-from llama_index.tools import QueryEngineTool, ToolMetadata
-from llama_index.agent import ReActAgent
-from llama_index.llms import OpenAI
-from pdf import lpp_engine, apg_engine, ifd_engine
-from mafs import multiply_tool, add_tool
+from llama_index.core import SimpleDirectoryReader, VectorStoreIndex
+from llama_index.llms.llama_cpp import LlamaCPP
+from llama_index.llms.llama_cpp.llama_utils import (
+    messages_to_prompt,
+    completion_to_prompt,
+)
 
-load_dotenv()
+llm = LlamaCPP(
+    # You can pass in the URL to a GGML model to download it automatically
+    model_path="models/Meta-Llama-3-8B-Instruct.Q4_1.gguf",
+    # optionally, you can set the path to a pre-downloaded model instead of model_url
+    temperature=0.1,
+    max_new_tokens=256,
+    # llama2 has a context window of 4096 tokens, but we set it lower to allow for some wiggle room
+    context_window=3900,
+    # kwargs to pass to __call__()
+    generate_kwargs={},
+    # kwargs to pass to __init__()
+    # set to at least 1 to use GPU
+    model_kwargs={"n_gpu_layers": 1},
+    # transform inputs into Llama2 format
+    messages_to_prompt=messages_to_prompt,
+    completion_to_prompt=completion_to_prompt,
+    verbose=True,
+)
 
-tools = [
-    note_engine,
-    QueryEngineTool(
-        query_engine=lpp_engine,
-        metadata=ToolMetadata(
-            name="lpp_data",
-            description="il s'agit de la loi suisse sur le droit de la prévoyace professionnelle vieillesse, survivants et invalidité", 
-        ),
-    ),
-    QueryEngineTool(
-        query_engine=apg_engine,
-        metadata=ToolMetadata(
-            name="apg_data",
-            description="ce document des inforamtions sur les allocations pour perte de gain dans le cadre de l'armée suisse", 
-        ),
-    ),
-    QueryEngineTool(
-        query_engine=ifd_engine,
-        metadata=ToolMetadata(
-            name="ifd_data",
-            description="il s'agit de la loi fédérale suisse sur l'impôt fédéral direct", 
-        ),
-    ),
-    multiply_tool,
-    add_tool,
-]
 
-llm = OpenAI(model="gpt-3.5-turbo")
-agent = ReActAgent.from_tools(tools, llm=llm, verbose=True, context=context)
+""" # will print the response once its done generating
+response = llm.complete("Hello! Can you tell me a poem about cats and dogs?")
+print(response.text) """
 
-while (prompt := input("Enter a prompt (q to quit): ")) != "q":
-    result = agent.query(prompt)
-    print(result)
+# prints while the response is beeing generated
+response_iter = llm.stream_complete("translate the following text to french: Die Spanngurten der Treppen abnehmen und in der grauen Kiste versorgen")
+for response in response_iter:
+    print(response.delta, end="", flush=True)
+    if response == completion_to_prompt:
+        break
